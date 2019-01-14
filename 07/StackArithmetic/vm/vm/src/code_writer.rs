4841ec97986aeaccbe3384ccb8d2861fecbe6ac4
yamagenii <bass.g.l42@gmail.com>
2019-01-14 22:39:34 +0900

    [07][add]add

--- 07/StackArithmetic/vm/vm/src/code_writer.rs
+++ 07/StackArithmetic/vm/vm/src/code_writer.rs
@@ -13,11 +13,34 @@ impl<'a> CodeWriter<'a> {
         }
     }
 
-    pub fn write_arithmetc(command: String) {
-
+    pub fn write_arithmetc(&mut self, command: String) {
+        if command.find("add") != None {
+            self.command.push("@SP".to_string());
+            self.command.push("A=M".to_string());
+            self.command.push("D=M".to_string());
+            self.command.push("@SP".to_string());
+            self.command.push("M=M-1".to_string());
+            self.command.push("A=M".to_string());
+            self.command.push("M=M+D".to_string());
+        }
     }
 
-    pub fn write_push_pop(command_type: parser::CommandType, segment: String, index: isize) {
+    pub fn write_push_pop(&mut self, command_type: parser::CommandType, segment: String, index: isize) {
+        match command_type {
+            parser::CommandType::CPush => {
+                if segment.find("constant") != None {
+                    self.command.push(format!("{}{}","@", index.to_string()).to_string());
+                    self.command.push("D=A".to_string());
+                    self.command.push("@SP".to_string());
+                    self.command.push("A=M".to_string());
+                    self.command.push("M=D".to_string());
+                    self.command.push("@SP".to_string());
+                    self.command.push("M=M+1".to_string());
+                }
+            }
+            _ => {}
+        }
+
     }
 }
 
